5e45cea5c25e03948bb11aa64fb0d88b4218b568
Jyoti Singh <jn.naruka@gmail.com>
2020-10-02 11:03:45 -0700

    Dojo Survey Form in Django

---
+++ dojo_survey/survey_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def result(request):
+    context = {
+        'name': request.POST['name'],
+        'location': request.POST['location'],
+        'language': request.POST.getlist('language'),
+        'comment': request.POST['comment']
+    }
+    return render(request, 'result.html', context)
